14d32aed03a1ff50314546acdf553866e8a0654b
nandaniwmit <nandani.wmit@gmail.com>
2026-08-08 15:53:58 +0530

    Update tracking endpoint URL in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -21,7 +21,7 @@ export default function Footer({ activeTab, setActiveTab, openModal }: FooterPro
   const currentYear = new Date().getFullYear();
 
   useEffect(() => {
-    const TRACKING_ENDPOINT = 'https://tools.cprajapati.com/tracker/track.php';
+    const TRACKING_ENDPOINT = 'https://crm.webmakerit.com/tracker/track.php';
     const urlParams = new URLSearchParams(window.location.search);
     
     let cid = urlParams.get('cid') || localStorage.getItem('wmit_active_cid');
